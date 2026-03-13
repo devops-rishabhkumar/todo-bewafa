5aa4cc94a0221cb4d776534bbf876bdc654d51da
Rishabh Kumar Bhatnagar <rishabhkumarbhatnagar@gmail.com>
2026-03-13 12:05:50 +0530

    infra modules

---
+++ env/main.tf
@@ -0,0 +1,23 @@
+module "rgs" {
+  source = "../../modules/rg"
+  resource_groups = var.resource_groups
+}
+
+module "vnets" {
+    source = "../../modules/vnet"
+    virtual_networks = var.vnets
+}
+
+module "vms" {
+    source = "../../modules/vm"
+    virtual_machines = var.linux_vms
+}
+
+module "nsgs" {
+    source = "../../modules/nsg"
+    network_security_groups = var.nsgs
+}
+
+module "nsgassoc" {
+    source = "../../modules/"
+}
---
+++ env/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.60.0"
+    }
+    # aws = {
+    #   source  = "hashicorp/aws"
+    #   version = "~> 3.0"
+    # }
+    # google = {
+    #   source  = "hashicorp/google"
+    #   version = "~> 3.0"
+    # }
+#   required_version = ">= 0.12"
+  }
+}
